a8990f5a019ea7f8d3b0fee7b1289d451d903e86
Jmarchandev <j.marchand.dev@gmail.com>
2021-09-03 10:39:38 +0200

    app.js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(port, () => {
     console.log(`server listening on ${port}`);
